cba03b9a100607852d686c5f97a74c8b9fffdb21
Victor Crudu <vcrudu@hotmail.com>
2023-09-01 13:56:12 +0100

    Added contact us

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,67 +1,114 @@
-"use client"
-import Link from 'next/link'
-import { useEffect, useState } from 'react'
-import { cn } from '@/lib/utils'
+"use client";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
+import { X, Menu } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
 
 export default function Header() {
-    const [menuOpen, setMenuOpen] = useState(false);
-    const [headerBackground, setHeaderBackground] = useState('bg-transparent');
-    const [textColor, setTextColor] = useState('text-white');
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [headerBackground, setHeaderBackground] = useState("bg-transparent");
+  const [textColor, setTextColor] = useState("text-white");
 
-    useEffect(() => {
-        if (typeof window !== "undefined") {
-
-            const handleScroll = () => {
-                const position = window.scrollY;
-                if (position < 100) {
-                    setHeaderBackground('bg-transparent');
-                    setTextColor('text-white');
-                } else {
-                    setHeaderBackground('bg-slate-300');
-                    setTextColor('text-black');
-                }
-                console.log(position)
-            }
-
-            window.addEventListener("scroll", handleScroll);
-
-            return () => window.removeEventListener("scroll", handleScroll);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const handleScroll = () => {
+        const position = window.scrollY;
+        if (position < 100) {
+          setHeaderBackground("bg-transparent");
+          setTextColor("text-white");
+        } else {
+          setHeaderBackground("bg-slate-300");
+          setTextColor("text-black");
         }
-    }, [])
+        console.log(position);
+      };
 
-    return (
-        <header className={cn('fixed w-full h-20 flex flex-row justify-between md:justify-start z-40 px-4 top-0', headerBackground, textColor)}>
-            <div className='flex flex-col justify-center flex-nowrap font-bold text-3xl'>
-                <Link className='flex flex-row justify-center w-32' href='#'>R-Think</Link>
+      window.addEventListener("scroll", handleScroll);
+
+      return () => window.removeEventListener("scroll", handleScroll);
+    }
+  }, []);
+
+  return (
+    <header
+      className={cn(
+        "fixed w-full h-20 flex flex-row justify-between md:justify-start z-40 px-4 top-0",
+        headerBackground,
+        textColor
+      )}
+    >
+      <div className="flex flex-col justify-center flex-nowrap font-bold text-3xl">
+        <Link className="flex flex-row justify-center w-32" href="#">
+          R-Think
+        </Link>
+      </div>
+      <nav className="hidden md:flex flex-col justify-center text-lg ml-20">
+        <ul className="md:flex md:flex-row justify-center">
+          <li className="px-4 hover:underline">
+            <Link href="/">About</Link>
+          </li>
+          <li className="px-4 hover:underline">
+            <Link href="/">Blog</Link>
+          </li>
+          <li className="px-4 hover:underline">
+            <Link href="/">Careers</Link>
+          </li>
+          <li className="px-4 hover:underline">
+            <Link href="/">Statute</Link>
+          </li>
+          <li className="px-4 hover:underline">
+            <a href="mailto:info@rthink.ai">Contact us</a>
+          </li>
+        </ul>
+      </nav>
+      <div className="flex flex-col justify-center flex-nowrap text-lg">
+        <button
+          className="md:hidden flex flex-row justify-center w-28 font-bold hover:cursor-pointer"
+          onClick={() => {
+            setMenuOpen((x) => !x);
+          }}
+        >
+          <Menu size={36} />
+        </button>
+      </div>
+      {menuOpen ? (
+        <div className="absolute left-0 w-screen text-black">
+          <div className="flex flex-col justify-center items-center pt-6 md:hidden bg-slate-300">
+            <div className="pl-4 flex flex-col self-start flex-nowrap font-bold text-3xl">
+              <Link className="flex flex-row w-32" href="#">
+                R-Think
+              </Link>
             </div>
-            <nav className='hidden md:flex flex-col justify-center text-lg ml-20'>
-                <ul className='md:flex md:flex-row justify-center'>
-                    <li className='px-4 hover:underline'><Link href='/'>About</Link></li>
-                    <li className='px-4 hover:underline'><Link href='/'>Blog</Link></li>
-                    <li className='px-4 hover:underline'><Link href='/'>Careers</Link></li>
-                    <li className='px-4 hover:underline'><Link href='/'>Statute</Link></li>
-                </ul>
-            </nav>
-            <div className='flex flex-col justify-center flex-nowrap text-lg'>
-                <button className='md:hidden flex flex-row justify-center w-28 font-bold hover:cursor-pointer' onClick={() => { setMenuOpen(x => !x) }}>Menu</button>
-            </div>
-            {
-                menuOpen ?
-                    <div className='absolute w-full'>
-                        <div className='flex flex-col justify-center items-center bg-white md:hidden'>
-                            <div className='pl-4 flex flex-col self-start flex-nowrap font-bold text-3xl'>
-                                <Link className='flex flex-row w-32' href='#'>R-Think</Link>
-                            </div>
-                            <ul className='w-full flex flex-col justify-center text-base'>
-                                <li className='h-10 flex items-center justify-center hover:bg-sky-300'><Link href='/'>About</Link></li>
-                                <li className='h-10 flex items-center justify-center hover:bg-sky-300'><Link href='/'>Blog</Link></li>
-                                <li className='h-10 flex items-center justify-center hover:bg-sky-300'><Link href='/'>Careers</Link></li>
-                                <li className='h-10 flex items-center justify-center hover:bg-sky-300'><Link href='/'>Statute</Link></li>
-                            </ul>
-                            <div className='relative hover:underline hover:cursor-pointer bottom-[230px] left-48' onClick={() => { setMenuOpen(x => !x) }}>Close</div>
-                        </div>
-                    </div> : null
-            }
-        </header>
-    )
+            <ul className="w-full flex flex-col justify-center text-base">
+              <li className="h-10 flex items-center justify-center hover:bg-sky-300">
+                <Link href="/">About</Link>
+              </li>
+              <li className="h-10 flex items-center justify-center hover:bg-sky-300">
+                <Link href="/">Blog</Link>
+              </li>
+              <li className="h-10 flex items-center justify-center hover:bg-sky-300">
+                <Link href="/">Careers</Link>
+              </li>
+              <li className="h-10 flex items-center justify-center hover:bg-sky-300">
+                <Link href="/">Statute</Link>
+              </li>
+              <li className="h-10 flex items-center justify-center hover:bg-sky-300">
+                <Link href="/">Contact us</Link>
+              </li>
+            </ul>
+          </div>
+          <div
+            className="absolute hover:underline hover:cursor-pointer top-[30px] right-4"
+            onClick={() => {
+              setMenuOpen((x) => !x);
+            }}
+          >
+            <X size={36} />
+          </div>
+          <Separator></Separator>
+        </div>
+      ) : null}
+    </header>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,64 @@
+import { Separator } from "@/components/ui/separator";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="flex flex-col justify-start">
       <video src="/rthink.mp4" muted autoPlay loop playsInline></video>
-      <div className="relative bottom-48 left-10 md:bottom-80 md:left-20">
-        <div className="absolute text-white w-4/6 md:w-8/12 text-[40px] md:text-[60px]"> 
+      <div className="relative bottom-36 left-10 md:bottom-56 md:left-20">
+        <div className="absolute text-white w-4/6 md:w-8/12 text-[30px] md:text-[40px]">
           We Elevate Intelligence for Your System.
         </div>
       </div>
-      <div className="flex flex-row justify-evenly items-center mt-16">
-        <div className="md:text-[40px] w-2/6 px-8">
+      <div className="flex flex-col md:flex-row justify-evenly items-center mt-16 mb-10">
+        <div className="text-center mb-8 font-bold text-[50px] w-80 px-8">
           About us
         </div>
         <div className="w-6/12">
-        At R-Think, we are obsessed with the fusion of traditional IT solutions and avant-garde AI technologies. Our aim? To position your business at the cutting edge of technological evolution.
+          At R-Think, we are obsessed with the fusion of traditional IT
+          solutions and avant-garde AI technologies. Our aim? To position your
+          business at the cutting edge of technological evolution.
+        </div>
+      </div>
+      <Separator className="bg-black"></Separator>
+      <div className="flex flex-col md:flex-row mt-10">
+        <div className="flex flex-col md:w-4/12 ml-5 mt-5">
+          <div className="font-bold">Our Team at R-Think Software</div>
+          <div className="mt-5 mb-5">
+            We're a nimble group of software engineers and data scientists. Our
+            forte? Infusing AI into traditional software, especially when faced
+            with:
+          </div>
+          <div className="ml-4 mb-2">
+            <b>Big Challenges:</b> Those that demand deep dives and effort.
+          </div>
+          <div className="ml-4 mb-2">
+            <b>Evolving Issues:</b> Problems that change as days go by.
+          </div>
+          <div className="ml-4 mb-2">
+            <b>Time-Shifted Puzzles:</b> Where the right answers aren't always
+            the same.
+          </div>
+          <div className="ml-4 mb-5">
+            <b>Boundary-Pushers:</b> Tasks that make us rethink the possible.
+          </div>
+          <div>
+            Give us a challenge, and we'll engineer a smart solution for you.
+          </div>
+        </div>
+        <div className="mt-4 md:mt-0">
+          <img src="/office.jpeg" />
+        </div>
+      </div>
+      <Separator className="bg-black mt-10"></Separator>
+      <div className="flex flex-col md:flex-row justify-center md:justify-around mt-4 mb-40 md:ml-10">
+        <div className="text-center">R-Think Software© 2023</div>
+        <div>
+          <ul className="w-full flex flex-col justify-center text-base">
+            <li className="h-10 flex items-center justify-center hover:bg-sky-300">
+              <a href="mailto:info@rthink.ai">Contact us</a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
